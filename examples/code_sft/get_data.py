59f784297e4e53e526b0ba6dff5bdc4e75b45516
coobiw <bowenqu@stu.pku.edu.cn>
2024-10-20 16:39:52 +0800

    add script to get magicoder-evol-instruct-110k dataset in aria-format

---
+++ examples/code_sft/get_data.py
@@ -0,0 +1,28 @@
+import os
+import json
+from tqdm import trange
+
+from datasets import load_dataset
+
+save_dir = './datasets/code_sft'
+os.makedirs(save_dir, exist_ok=True)
+
+dataset = load_dataset("ise-uiuc/Magicoder-Evol-Instruct-110K")['train']
+l_ds = len(dataset)
+
+with open(os.path.join(save_dir, "train.jsonl"),"w") as fo:
+    for i in trange(l_ds):
+        instruction, response = dataset[i]['instruction'], dataset[i]['response']
+        item = {
+            "messages": [
+                    {
+                        "content": [{"text": instruction, "type": "text"},],
+                        "role": "user"
+                    },
+                    {
+                        "content": [{"text": response, "type": "text"},],
+                        "role": "assistant"
+                    },
+                ],
+        }
+        fo.write(f"{json.dumps(item, ensure_ascii=False)}\n")
